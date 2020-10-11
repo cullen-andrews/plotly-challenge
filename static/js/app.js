fbc4dbbfef706be37338065af8df3a49fd20132c
Cullen Andrews <66756039+cullen-andrews@users.noreply.github.com>
2020-10-10 18:54:37 -0700

    setup directories

---
+++ static/js/app.js
@@ -0,0 +1,5 @@
+// Use D3 fetch to read the JSON file
+// The data from the JSON file is arbitrarily named importedData as the argument
+d3.json("samples.json").then(data => {
+    console.log(data);
+})
