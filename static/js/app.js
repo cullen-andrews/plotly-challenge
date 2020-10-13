38df5a83b255b2190a434d888c4dca64dcf563fd
Cullen Andrews <66756039+cullen-andrews@users.noreply.github.com>
2020-10-12 17:13:13 -0700

    setup function init

--- static/js/app.js
+++ static/js/app.js
@@ -1,5 +1,12 @@
 // Use D3 fetch to read the JSON file
 // The data from the JSON file is arbitrarily named importedData as the argument
-d3.json("samples.json").then(data => {
-    console.log(data);
-})
+function init() {
+    d3.json("samples.json").then(data => {
+        console.log(data);
+        })
+    }
+
+
+
+
+init();
