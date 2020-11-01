5a6f40f03f49e0d9ada245bc96f50d93ad58b2e6
Cullen Andrews <66756039+cullen-andrews@users.noreply.github.com>
2020-11-01 12:19:34 -0800

    Clean up. Rebuild. Dropdown built.

--- static/js/app.js
+++ static/js/app.js
@@ -1,47 +1,38 @@
-// Use D3 fetch to read the JSON file
-// The data from the JSON file is arbitrarily named importedData as the argument
+
+// Build the bar chart
+function bar(){}
+
+// Build the bubble chart
+function bubble(){}
+
+// Build the demographics panel
+function demographics(){}
+
 function init() {
+  // Use D3 to select the dropdown menu. See class activity Ins_Dropdown_Events
+    var dropSelect = d3.select("#selDataset");
+//   console.log(dropSelect);
+  
     d3.json("samples.json").then(data => {
         console.log(data);
-        })
-    }
 
-// Call updatePlotly() when a change takes place to the DOM
-d3.selectAll("#selDataset").on("change", updatePlotly);
+        var names = data.names;
+        console.log(names);
+        var metadata = data.metadata;
+        console.log(metadata);
+        var samples = data.samples;
+        console.log(samples);
+        
+        // Appending the sample names to the dropdown menu
+        names.forEach((name) => {
+            dropSelect.append("option").text(name).property("value", name);
+        });
 
-// This function is called when a dropdown menu item is selected
-function updatePlotly() {
-    // Use D3 to select the dropdown menu
-    var dropdownMenu = d3.select("#selDataset");
-    // Assign the value of the dropdown menu option to a variable
-    var subjectID = dropdownMenu.property("value");
-  
-    // Initialize x and y arrays
-    var x = [];
-    var y = [];
-  
-    if (subjectID === '940') {
-      x = [1, 2, 3, 4, 5];
-      y = [1, 2, 4, 8, 16];
-    }
-  
-    if (subjectID === '941') {
-      x = [10, 20, 30, 40, 50];
-      y = [1, 10, 100, 1000, 10000];
-    }
-  
-    console.log(x);
-    console.log(y);
+    });
 
-    // // Note the extra brackets around 'x' and 'y'
-    // Plotly.restyle("plot", "x", [x]);
-    // Plotly.restyle("plot", "y", [y]);
-  }
-  
+}
 
-function optionChanged(selectObject) {
-    var dropSelect = selectObject.value;
-    console.log(dropSelect);
-} 
+function optionChanged(option) {}
+
 
 init();
